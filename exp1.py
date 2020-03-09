9b6ee007bfc55dea80ce70b1f5010e897e3a64f9
dheeraj7596 <dheeraj7596.dm@gmail.com>
2020-03-08 17:45:02 -0700

    Images rendered.

--- exp1.py
+++ exp1.py
@@ -1,32 +1,39 @@
-from PIL import Image
+import cv2
 import numpy as np
 from scipy.fftpack import idct, dct
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     data_path = "./data/"
-    img = Image.open(data_path + "lena.bmp")
-    img_array = np.asarray(img)/255
+    img_array = cv2.imread(data_path + "lena.bmp", 0)
+    # img = Image.fromarray(img_array)
     shape = img_array.shape
-    plt.imshow(img,cmap='gray')
-    
-    
-    theta = dct(img_array, type=2)
-    
-    img_back = idct(dct(img_array))
     plt.figure()
-    plt.imshow(img_back,cmap='gray')
+    plt.title("Original Image")
+    plt.imshow(img_array, cmap='gray')
+    plt.show()
+
+    theta = dct(img_array, type=2, norm="ortho")
+
+    img_back = idct(dct(img_array, norm="ortho"), norm="ortho")
+    plt.figure()
+    plt.title('Reconstructed with all coeff')
+    plt.imshow(img_back, cmap='gray')
+    plt.show()
+
     U = np.absolute(theta).flatten()
 
     U_sorted = sorted(U, reverse=True)
     plt.figure()
+    plt.title("DCT coefficients")
     plt.plot(range(len(U_sorted)), U_sorted)
     plt.show()
 
     plt.figure()
-    thresh = U_sorted[200000]
+    plt.title('Reconstructed with some coeff')
+    thresh = U_sorted[22000]
     theta[np.absolute(theta) < thresh] = 0
     X_cap = idct(theta, type=2)
-    im = Image.fromarray(X_cap)
-    plt.imshow(im,cmap='gray')
+    plt.imshow(X_cap, cmap='gray')
+    plt.show()
     pass
