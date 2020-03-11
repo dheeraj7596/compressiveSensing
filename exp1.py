2b94c730dbd1f7f514dccc05f8d476b2fddb4f5a
udayan14 <14udayanjoshi@gmail.com>
2020-03-10 18:24:41 -0700

    Changed dct to dct2

--- exp1.py
+++ exp1.py
@@ -3,9 +3,22 @@ import numpy as np
 from scipy.fftpack import idct, dct
 import matplotlib.pyplot as plt
 
+
+def getDCTBasis(n):
+    '''Gives us the basis for 1d-DCT for n-dimensional signals'''
+    return dct(np.eye(n),type=2,norm='ortho')
+
+def dct2 (block):
+  return dct(dct(block.T, norm = 'ortho').T, norm = 'ortho')
+
+def idct2 (block):
+  return idct(idct(block.T, norm = 'ortho').T, norm = 'ortho')
+
 if __name__ == "__main__":
     data_path = "./data/"
     img_array = cv2.imread(data_path + "lena.bmp", 0)
+    assert img_array.shape[0]==img_array.shape[1]
+    n = img_array.shape[0]
     # img = Image.fromarray(img_array)
     shape = img_array.shape
     plt.figure()
@@ -13,27 +26,25 @@ if __name__ == "__main__":
     plt.imshow(img_array, cmap='gray')
     plt.show()
 
-    theta = dct(img_array, type=2, norm="ortho")
+    theta = dct2(img_array)
+    Phi = getDCTBasis(n)
+    theta2 = Phi.T@img_array@Phi
 
-    img_back = idct(dct(img_array, norm="ortho"), norm="ortho")
-    plt.figure()
-    plt.title('Reconstructed with all coeff')
-    plt.imshow(img_back, cmap='gray')
-    plt.show()
-
+    
     U = np.absolute(theta).flatten()
 
     U_sorted = sorted(U, reverse=True)
     plt.figure()
-    plt.title("DCT coefficients")
+    plt.title("DCT2 coefficients")
     plt.plot(range(len(U_sorted)), U_sorted)
     plt.show()
 
     plt.figure()
-    plt.title('Reconstructed with some coeff')
-    thresh = U_sorted[22000]
+    s = 20000
+    plt.title('Reconstructed with {0} coefficients out of {1}'.format(s,n*n))
+    thresh = U_sorted[s]
     theta[np.absolute(theta) < thresh] = 0
-    X_cap = idct(theta, type=2)
+    X_cap = idct2(theta)
     plt.imshow(X_cap, cmap='gray')
     plt.show()
     pass
---
+++ exp2.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 10:10:33 2020
+
+@author: 14uda
+"""
+
+import numpy as np
+from scipy.fftpack import idct, dct
+
+x = np.random.randn((100))
+
+theta = dct(x,type=2,norm='ortho')
+
+Phi = dct(np.eye(100),type=2,norm='ortho')
+
+theta2 = Phi.T@x
